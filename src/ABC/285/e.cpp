f467cde95248f957fad53a67502df1baaa22fc02
ituki <ituki@usernoMacBook-Pro.local>
2023-06-11 01:29:55 +0900

    d問題とe問題をいくつか解いた

---
+++ src/ABC/285/e.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+using ll = long long;
+int main(void) {
+    int n;
+    cin >> n;
+    vector<ll> a(n + 1);
+    rep(i, n) cin >> a[i + 1];
+    vector<ll> b(n + 1);
+    rep(w, n + 1) {
+        for (int i = 1; i < w; i++) {
+            int j = w - i;
+        }
+    }
+}
