0d3974cc349b39a82970710b590a8ed63450c561
ituki <ituki@usernoMacBook-Pro.local>
2023-06-14 13:01:20 +0900

    dpとspanning treeの問題をいくつか解いた

---
+++ src/ABC/235/d.cpp
@@ -0,0 +1,2 @@
+// Multiply and Rotate (緑色, 400 点)
+// BFSを使って解く
--- src/Algo/Graph/sp_tree/algo_1.cpp
+++ src/Algo/Graph/sp_tree/algo_1.cpp
@@ -1,46 +1,43 @@
 #include <algorithm>
 #include <iostream>
-#include <utility>
 #include <vector>
 using namespace std;
 using ll = long long;
+struct Edge {
+    int u;
+    int v;
+    ll cost;
+    bool operator<(const Edge& other) const { return (cost < other.cost); }
+};
 class UnionFind {
    public:
     UnionFind() = default;
-    explicit UnionFind(int n) : m_parentsOrSize(n, -1) {}
+    explicit UnionFind(size_t n) : m_parentsOrSize(n, -1) {}
     int find(int i) {
         if (m_parentsOrSize[i] < 0) return i;
-        return m_parentsOrSize[i] = find(m_parentsOrSize[i]);
+        return (m_parentsOrSize[i] = find(m_parentsOrSize[i]));
     }
     void merge(int a, int b) {
+        a = find(a);
+        b = find(b);
         if (a != b) {
             if (-m_parentsOrSize[a] < -m_parentsOrSize[b]) swap(a, b);
             m_parentsOrSize[a] += m_parentsOrSize[b];
             m_parentsOrSize[b] = a;
         }
     }
-    bool connected(int a, int b) { return find(a) == find(b); }
+    bool connected(int a, int b) { return (find(a) == find(b)); }
     int size(int i) { return -m_parentsOrSize[find(i)]; }
 
    private:
     vector<int> m_parentsOrSize;
-};
-struct Edge {
-    int u;
-    int v;
-    ll cost;
-    bool operator<(const Edge& e) const { return this->cost < e.cost; }
 };
 using Graph = vector<Edge>;
 int main(void) {
     int V, E;
     cin >> V >> E;
     Graph edges(E);
-    for (auto& edge : edges) {
-        cin >> edge.u >> edge.v >> edge.cost;
-        --edge.u;
-        --edge.v;
-    }
+    for (auto& edge : edges) cin >> edge.u >> edge.v >> edge.cost;
     sort(edges.begin(), edges.end());
     UnionFind uf(V);
     ll sum = 0;
